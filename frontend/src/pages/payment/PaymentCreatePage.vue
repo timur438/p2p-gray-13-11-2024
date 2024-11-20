9511bfd443881ce63695874f21890411cb6d2669
ThunderAl <work@thunderal.net>
2024-11-21 00:22:30 +0100

    Translate

--- frontend/src/pages/payment/PaymentCreatePage.vue
+++ frontend/src/pages/payment/PaymentCreatePage.vue
@@ -3,8 +3,8 @@
     <div
         class="flex flex-col gap-[13px] w-[430px] p-[26px_18px] rounded-[12px] shadow-[0_2px_17.5px_0_rgba(255,255,255,0.05)] border border-[rgba(255,255,255,0.1)] bg-[#0e0e0e]">
       <div class="flex flex-col gap-3">
-        <input type="number" v-model="price" placeholder="Price" class="form-input">
-        <button class="btn-primary" @click="createPayment">Create payment</button>
+        <input type="number" v-model="price" placeholder="Цена" class="form-input">
+        <button class="btn-primary" @click="createPayment">Создать платеж</button>
       </div>
     </div>
   </div>
