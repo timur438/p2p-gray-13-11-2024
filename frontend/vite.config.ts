c7fa1be4eeb03f1f16496fe22c956413ee6c0c3e
ThunderAl <work@thunderal.net>
2024-11-20 23:56:22 +0100

    Refactor and add logo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import tailwindcssNesting from 'tailwindcss/nesting'
 import autoprefixer from 'autoprefixer'
 import postcssImport from 'postcss-import'
 
+const isBuild = process.argv.includes('build')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -16,6 +18,16 @@ export default defineConfig({
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '/user-api'),
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        sourcemapExcludeSources: isBuild,
+        entryFileNames: isBuild ? 'assets/[hash].js' : 'assets/[name].js',
+        assetFileNames: isBuild ? 'assets/[hash][extname]' : 'assets/[name]-[hash][extname]',
+        chunkFileNames: isBuild ? 'assets/[hash].js' : 'assets/[name].js',
       },
     },
   },
